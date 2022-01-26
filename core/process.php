918f0cbeebcc20ac10b912e854e5cb02ae8919a2
kekecoder <abimbola.felix@gmail.com>
2022-01-26 13:17:49 +0100

    initial commit

---
+++ core/process.php
@@ -0,0 +1,57 @@
+<?php 
+$error = [];
+define("ERROR_MSG", "Field cannot be Empty");
+$firstname = validatInput($_POST['firstname']);
+
+$lastname = validatInput($_POST['lastname']);
+
+$email = validatInput($_POST['email']);
+
+$comment = validatInput($_POST['comment']);
+
+if(!$firstname){
+  $error['firstname'] = ERROR_MSG;
+}
+
+if(!$lastname){
+  $error['lastname'] = ERROR_MSG;
+}
+
+if(!$email){
+  $error['email'] = ERROR_MSG;
+}elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $error['email'] = "Email is not valid";
+}
+
+if(!$comment){
+  $error['comment'] = ERROR_MSG;
+}elseif(strlen($comment) < 10){
+  $error['comment'] = "The text you entered is too short";
+}
+
+$created_at = date("Y-m-d H:i:s");
+
+if(empty($error)){
+  try {
+    require_once 'dbconfig.php';
+    require_once 'controller.php';
+  
+  $insert = new Comment($pdo, 'guest');
+  $insert->insert(['firstname' => $firstname, 'lastname' => $lastname, 'email' => $email, 'comment' => $comment, 'created_at' => $created_at]);
+  header("Location: ../guestapp.php");
+    
+  } catch (Exception $e) {
+    //print "An Exception has occurred " . $e->getMessage();
+    print "Server is down at this time 😪😪, please try again later";
+    exit();
+  }
+}
+
+function validatInput($data){
+  $data = trim($data);
+  $data = htmlspecialchars($data);
+  $data = strtolower($data);
+  $data = ucfirst($data);
+  
+  return $data;
+}
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+ if($_SERVER['REQUEST_METHOD'] === "POST"){
+   require_once __DIR__ . '/core/process.php';
+ }
+?>
+<!Doctype HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf8">
+    <title>Guestbook</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container container-sm">
+      <h1 class="text-center mb-3 mt-3">Welcome on Board</h1>
+      <p>Please fill the form below.</p>
+      <form action="" method="post">
+        <div class="row">
+          <div class="col">
+            <div class="form-group">
+          <label for="firstname">First Name</label>
+          <input type="text" name="firstname" class="form-control <?php echo isset($error['firstname']) ? 'is-invalid' : ''?>" value="<?=$firstname ?? ''?>">
+          <small class="invalid-feedback">
+            <?php echo $error['firstname'] ?? ''?>
+          </small>
+            </div>
+          </div>
+          <div class="col">
+            <div class="form-group">
+          <label for="lastname">Last Name</label>
+          <input type="text" class="form-control <?php echo isset($error['lastname']) ? 'is-invalid' : ''?>" name="lastname" value="<?=$lastname ?? ''?>">
+          <small class="invalid-feedback">
+            <?php echo $error['lastname'] ?? ''?>
+          </small>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" class="form-control <?php echo isset($error['email']) ? 'is-invalid' : ''?>" name="email" value="<?=$email ?? ''?>">
+          <small class="invalid-feedback">
+            <?php echo $error['email'] ?? ''?>
+          </small>
+        </div>
+        <div class="form-group">
+          <label for="comment">Comment</label>
+          <textarea name="comment" class="form-control <?php echo isset($error['comment']) ? 'is-invalid' : ''?>"><?=$comment ?? ''?></textarea>
+          <small class="invalid-feedback">
+            <?php echo $error['comment'] ?? ''?>
+          </small>
+        </div>
+        <button class="btn btn-success">Add Comment</button>
+      </form>
+    </div>
+  </body>
+</html>
