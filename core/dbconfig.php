1b6b5075adc3f2c723304bcdc3e1ab4cf6f2079e
kekecoder <abimbola.felix@gmail.com>
2022-01-28 00:31:28 +0100

    remotesql.com

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -1,12 +1,10 @@
 <?php
 
 try {
-  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-  $server = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $db = substr($url["path"], 1);
+  $server = 'remotemysql.com';
+  $dbname = '6V3DUWFCoy';
+  $user = '6V3DUWFCoy';
+  $pass = 'G4ZOUl2GLh';
 
     $pdo = new PDO("mysql:host=$server; dbname=$username; charset=utf8mb4;", $password, $db);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
