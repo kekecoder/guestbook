9cf6af6e5225397158f44bf3f419c0cb69ddcd79
kekecoder <abimbola.felix@gmail.com>
2022-01-28 00:36:55 +0100

    updated

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -6,7 +6,7 @@ try {
   $user = '6V3DUWFCoy';
   $pass = 'G4ZOUl2GLh';
 
-    $pdo = new PDO("mysql:host=$server; dbname=$dbname; charset=utf8mb4;", $pass, $user);
+    $pdo = new PDO("mysql:host=$server; dbname=$dbname; charset=utf8mb4;", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     print "connected successfully";
