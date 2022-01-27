8ea6e1ffd53f6df0c5ed5298e1efb5b61276e0a9
kekecoder <abimbola.felix@gmail.com>
2022-01-27 23:40:54 +0100

    new one

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -1,22 +1,13 @@
 <?php
 
 try {
-  $url = parse_url(getenv("mysql://b442a4f3a7b6b1:f8c1776a@us-cdbr-east-05.cleardb.net/heroku_dc3cd6ea7276ac9?reconnect=true"));
-  $server = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $db = substr($url["path"], 1);
-  //$char = 'charset=utf8mb4';
-
-$pdo = new PDO($server, $username, $password, $db);
-    
+    $pdo = new PDO('mysql:host=us-cdbr-east-05.cleardb.net; port=3306; dbname=heroku_dc3cd6ea7276ac9?reconnect=true; charset=utf8mb4;', 'b442a4f3a7b6b1', 'f8c1776a');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     print "connected successfully";
 } catch (Error $e) {
-    print "An Exception has occured " . $e->getMessage() . '<br>';
+    //print "An Exception has occured " . $e->getMessage();
     print "Something went wrong, please try again later.";
 }
-
 
 
