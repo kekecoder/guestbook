3a75b81c57073bb3675703946f6dd017249f8bcc
kekecoder <abimbola.felix@gmail.com>
2022-01-27 23:49:27 +0100

    better version

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -1,7 +1,14 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=us-cdbr-east-05.cleardb.net; port=3306; dbname=heroku_dc3cd6ea7276ac9?reconnect=true; charset=utf8mb4;', 'b442a4f3a7b6b1', 'f8c1776a');
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+    $pdo = new PDO("mysql:host=$server; port=3306; dbname=$username; charset=utf8mb4;", $db, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     print "connected successfully";
