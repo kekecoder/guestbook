8c2ac866017cf886b996456c40e0a30d05cb0e5c
kekecoder <abimbola.felix@gmail.com>
2022-01-27 23:01:18 +0100

    corrected some errors

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -8,13 +8,13 @@ try {
   $db = substr($url["path"], 1);
   $char = 'charset=utf8mb4';
 
-$pdo = new mysqli($server, $username, $char, $password, $db);
+$pdo = new PDO($server, $username, $char, $password, $db);
     
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     print "connected successfully";
 } catch (Error $e) {
-    //print "An Exception has occured " . $e->getMessage();
+    print "An Exception has occured " . $e->getMessage() . '<br>';
     print "Something went wrong, please try again later.";
 }
 
