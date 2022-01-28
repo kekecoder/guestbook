44c1079acfb0226012ef6e8e0df09bd178e676c0
kekecoder <abimbola.felix@gmail.com>
2022-01-28 03:42:08 +0100

    updated

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -7,14 +7,14 @@ $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-
-$pdo = new PDO("mysql:host=$server;port=3306; dbname=$username; charset=utf8mb4", $password, $db);
+    $pdo = new PDO("mysql:host=$server; port=3306; dbname=$username; charset=utf8mb4;", $db, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    print "connected successfully";
+    //print "connected successfully";
 } catch (Error $e) {
-    print "An Exception has occured " . $e->getMessage();
+    //print "An Exception has occured " . $e->getMessage();
     print "Something went wrong, please try again later.";
 }
+
 
 
