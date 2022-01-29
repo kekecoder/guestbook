ed3d1b38b60c09665e9c9a4fbecb1dc7de2b9551
kekecoder <abimbola.felix@gmail.com>
2022-01-29 22:11:31 +0100

    jwd database

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -1,18 +1,19 @@
 <?php
 
 try {
-  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $url = getenv('JAWSDB_MARIA_URL');
+$dbparts = parse_url($url);
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-    $pdo = new PDO("mysql:host=$server; port=3306; dbname=$username; charset=utf8mb4;", $db, $password);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+    $pdo = new PDO("mysql:host=$hostname; port=3306; dbname=$username; ", $database, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    //print "connected successfully";
+    print "connected successfully";
 } catch (Error $e) {
-    //print "An Exception has occured " . $e->getMessage();
+    print "An Exception has occured " . $e->getMessage();
     print "Something went wrong, please try again later.";
 }
 
